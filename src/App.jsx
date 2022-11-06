73e7e43061105bde80355f2ea6ed0089745da308
HOHENSTAUF <47435631+HOHENSTAUF@users.noreply.github.com>
2022-11-07 01:45:01 +0400

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 
 import './App.css'
 
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -19,3 +21,12 @@ function App() {
 }
 
 export default App
+
+
+/* <div className="photo"></div>
+      <div className="main">
+        <div className="name">Albert</div>
+        <div className="profession">Duke of Prussia</div>
+        <button className="email"></button>
+      </div>
+      <div className="footer">email</div> */
